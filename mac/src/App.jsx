0eedf3f044ed52abfff2ea255a84cb55ff66efd3
Dipika <dipikac1306@gmail.com>
2026-08-09 15:15:58 +0530

    scroll effect

--- mac/src/App.jsx
+++ mac/src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './components/Hero'
 import ProductViewer from './components/ProductViewer'
 import gsap from 'gsap'
 import ScrollTrigger from 'gsap/src/ScrollTrigger'
+import Showcase from './components/Showcase'
 
 gsap.registerPlugin(ScrollTrigger)
 
@@ -12,6 +13,7 @@ const App = () => {
      <main> <NavBar />
      <Hero/> 
      <ProductViewer/>
+     <Showcase />
      </main>
   )
 }
